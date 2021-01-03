fe4fbcd93e9a36a204aa1d949ab6fba9cccb4890
DiabateJS <diabatejs@yahoo.fr>
2021-01-03 09:01:37 +0100

    Intégration du calcul des statistiques.

--- models/constants.php
+++ models/constants.php
@@ -8,12 +8,12 @@ class Constants {
     public static $ERROR_CODE = "ERROR";
 
     //SQL STATISTIQUE
-    public static $SQL_STATE_BY_USERS = "select u.fullname, t.etat, count(*) as tache from tache t, users u where t.idUser = u.id group by t.idUser, t.etat order by u.fullname";
-    public static $SQL_TASKS_BY_PROJECT = "select p.libelle as projet, count(*) as tache from tache t, projet p where t.idProjet = p.id group by idProjet";
-    public static $SQL_TASKS_BY_STATE = "select etat, count(*) as nbre from tache group by etat";
-    public static $SQL_PROJECTS_BY_STATE = "select etat, count(*) as nbre from projet group by etat";
-    public static $SQL_COUNT_TASKS = "select count(*) as nbre from tache";
-    public static $SQL_COUNT_PROJECTS = "select count(*) as nbre from projet";
+    public static $SQL_STATE_BY_USERS = "select u.fullname, t.etat, count(*) as tache from pm_tache t, pm_users u where t.idUser = u.id group by t.idUser, t.etat order by u.fullname";
+    public static $SQL_TASKS_BY_PROJECT = "select p.libelle as projet, count(*) as tache from pm_tache t, pm_projet p where t.idProjet = p.id group by idProjet";
+    public static $SQL_TASKS_BY_STATE = "select etat, count(*) as nbre from pm_tache group by etat";
+    public static $SQL_PROJECTS_BY_STATE = "select etat, count(*) as nbre from pm_projet group by etat";
+    public static $SQL_COUNT_TASKS = "select count(*) as nbre from pm_tache";
+    public static $SQL_COUNT_PROJECTS = "select count(*) as nbre from pm_projet";
 }
 
 ?>
